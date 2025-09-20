c741ff7bccf37880e374f051ed21fd5b8dbe7811
Arika Ishinami <delete0093@gmail.com>
2025-09-20 22:52:49 +0900

    add console sample

---
+++ example/Example.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Configuration.Writable;
+using Example.ConsoleApp;
+
+// initialize the writable config system
+// default save location is ./userconfig.json
+WritableConfig<SampleSetting>.Initialize();
+
+// if you want to specify a custom save location, use the following instead:
+//WritableConfig<SampleSetting>.Initialize(options =>
+//{
+//    options.FileName = "../custom-setting.json";
+//});
+
+// or use system app data folder
+// (e.g. C:\Users\<User>\AppData\Local\sample-console-app\appdata-setting.json on Windows):
+//WritableConfig<SampleSetting>.Initialize(options =>
+//{
+//    options.FileName = "appdata-setting.json";
+//    options.UseStandardSaveLocation("sample-console-app");
+//});
+
+// -------------------------------
+// get the config instance
+var sampleSetting = WritableConfig<SampleSetting>.GetValue();
+Console.WriteLine($">> Name: {sampleSetting.Name}, LastUpdatedAt: {sampleSetting.LastUpdatedAt}");
+
+// modify the config instance
+Console.Write(":: Enter new name: ");
+var newName = Console.ReadLine();
+
+// save the config instance
+sampleSetting.Name = newName;
+sampleSetting.LastUpdatedAt = DateTime.Now;
+WritableConfig<SampleSetting>.Save(sampleSetting);
+Console.WriteLine(":: Config saved.");
+
+// get updated config instance
+var updatedSampleSetting = WritableConfig<SampleSetting>.GetValue();
+Console.WriteLine(
+    $">> Name: {updatedSampleSetting.Name}, LastUpdatedAt: {updatedSampleSetting.LastUpdatedAt}"
+);
--- example/Example.WorkerService/Program.cs
+++ example/Example.WorkerService/Program.cs
@@ -3,19 +3,7 @@ using Example.WorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Add a writable configuration file located at the user config directory
-// e.g. "%LOCALAPPDATA%/Configuration.Writable.Examples/user-settings.json" on Windows
-builder
-    .AddUserConfigurationFile<SampleSetting>(options =>
-    {
-        options.InstanceName = "worker-sample-1";
-        options.Provider = new WritableConfigYamlProvider<SampleSetting>();
-    })
-    .AddUserConfigurationFile<SampleSetting>(options =>
-    {
-        options.InstanceName = "worker-sample-2";
-        options.Provider = new WritableConfigJsonProvider<SampleSetting>();
-    });
+builder.AddUserConfigurationFile<SampleSetting>();
 
 builder.Services.AddHostedService<Worker>();
 
