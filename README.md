65757f5832b12c2f12baa313f4aa1ab5d93e8285
Arika Ishinami <delete0093@gmail.com>
2025-09-20 14:10:23 +0900

    Fix formatting in IReadonlyOptions summary documentation

---
+++ README.md
@@ -0,0 +1,4 @@
+# Configuration.Writable
+`Microsoft.Extensions.Options`に書き込みができる`IOption<T>`を追加します。
+
+## 
--- src/Configuration.Writable/IReadonlyOptions.cs
+++ src/Configuration.Writable/IReadonlyOptions.cs
@@ -3,8 +3,7 @@
 namespace Configuration.Writable;
 
 /// <summary>
-/// Represents a read-only configuration options interface for accessing and monitoring options of type <typeparamref
-/// name="T"/>.
+/// Represents a read-only configuration options interface for accessing and monitoring options of type <typeparamref name="T"/>.
 /// </summary>
 /// <remarks>
 /// <see cref="IOptionsMonitor{T}"/> may not work properly in some environments (docker, network shares, etc.). <br/>
