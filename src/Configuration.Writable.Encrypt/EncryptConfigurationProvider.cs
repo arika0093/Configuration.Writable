9b91e5d5ff8e4c6e8dbf91d46b4047048ca37c22
Arika Ishinami <delete0093@gmail.com>
2025-09-22 01:05:09 +0900

    added

--- src/Configuration.Writable.Encrypt/EncryptConfigurationProvider.cs
+++ src/Configuration.Writable.Encrypt/EncryptConfigurationProvider.cs
@@ -15,18 +15,20 @@ internal class EncryptConfigurationProvider(EncryptConfigurationSource source)
         var iv = new byte[16];
 #if NET
         stream.ReadExactly(iv);
+#else
+        var bytesRead = 0;
+        while (bytesRead < iv.Length)
+        {
+            var read = stream.Read(iv, bytesRead, iv.Length - bytesRead);
+            if (read == 0)
+            {
+                throw new EndOfStreamException("Unexpected end of stream while reading IV.");
+            }
+            bytesRead += read;
+        }
 #endif
         var decryptor = aes.CreateDecryptor(source.Key, iv);
         using var cs = new CryptoStream(stream, decryptor, CryptoStreamMode.Read);
-        //var bytes = new List<byte>();
-        //int readByte = 0;
-        //while ((readByte = cs.ReadByte()) != -1)
-        //{
-        //    bytes.Add((byte)readByte);
-        //}
-        //var text = System.Text.Encoding.UTF8.GetString(bytes.ToArray());
-#if NET
         base.Load(cs);
-#endif
     }
 }
