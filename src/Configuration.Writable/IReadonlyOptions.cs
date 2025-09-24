5f6bc066d08aca20358c5ae4914b17ea5d7e00be
Arika Ishinami <delete0093@gmail.com>
2025-09-25 00:10:11 +0900

    Remove outdated remarks from IReadonlyOptions interface documentation for clarity

--- src/Configuration.Writable/IReadonlyOptions.cs
+++ src/Configuration.Writable/IReadonlyOptions.cs
@@ -5,10 +5,6 @@ namespace Configuration.Writable;
 /// <summary>
 /// Represents a read-only configuration options interface for accessing and monitoring options of type <typeparamref name="T"/>.
 /// </summary>
-/// <remarks>
-/// <see cref="IOptionsMonitor{T}"/> may not work properly in some environments (docker, network shares, etc.). <br/>
-/// <see cref="IReadonlyOptions{T}"/> is an interface to reliably obtain settings updated by <see cref="IWritableOptions{T}"/>.
-/// </remarks>
 public interface IReadonlyOptions<T> : IOptionsMonitor<T>
     where T : class
 {
