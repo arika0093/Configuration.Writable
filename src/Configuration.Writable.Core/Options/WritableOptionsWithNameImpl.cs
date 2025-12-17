6d09b8f0ae0761bd363eb73e05b8a43e54a41771
Arika Ishinami <delete0093@gmail.com>
2025-12-17 22:02:15 +0900

    fix

--- src/Configuration.Writable.Core/Options/WritableOptionsWithNameImpl.cs
+++ src/Configuration.Writable.Core/Options/WritableOptionsWithNameImpl.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Configuration.Writable;
 
 /// <summary>
-/// Implementation of WritableOptions with a specified standard name.
+/// Implementation of WritableOptions with a specified standard name. <br/>
 /// This is a wrapper to allow access as <c>[FromKeyedService("instanceName"))] IWritableOptions&lt;T&gt; ...</c>.
 /// </summary>
 /// <typeparam name="T">The type of the configuration class.</typeparam>
@@ -14,8 +14,7 @@ namespace Configuration.Writable;
 internal sealed class WritableOptionsWithNameImpl<T>(
     WritableOptionsImpl<T> innerWritableOptionsInstance,
     string instanceName
-)
- : IWritableOptions<T>, IDisposable
+) : IWritableOptions<T>, IDisposable
     where T : class, new()
 {
     /// <inheritdoc />
@@ -29,7 +28,7 @@ internal sealed class WritableOptionsWithNameImpl<T>(
         innerWritableOptionsInstance.GetConfigurationOptions(instanceName);
 
     /// <inheritdoc />
-    public IDisposable? OnChange(Action<T, string?> listener) => 
+    public IDisposable? OnChange(Action<T, string?> listener) =>
         innerWritableOptionsInstance.OnChange(listener);
 
     /// <inheritdoc />
@@ -37,6 +36,6 @@ internal sealed class WritableOptionsWithNameImpl<T>(
         innerWritableOptionsInstance.SaveAsync(instanceName, newConfig, cancellationToken);
 
     /// <inheritdoc />
-    public Task SaveAsync(Action<T> configUpdater, CancellationToken cancellationToken = default) => 
+    public Task SaveAsync(Action<T> configUpdater, CancellationToken cancellationToken = default) =>
         innerWritableOptionsInstance.SaveAsync(instanceName, configUpdater, cancellationToken);
 }
--- src/Configuration.Writable.Core/WritableOptionsCoreExtensions.cs
+++ src/Configuration.Writable.Core/WritableOptionsCoreExtensions.cs
@@ -14,9 +14,21 @@ public static class WritableOptionsCoreExtensions
     /// </summary>
     /// <typeparam name="T">The type of the options class to configure. This type must be a reference type.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/> to which the writable options services will be added.</param>
+    /// <returns>The <see cref="IServiceCollection"/> instance with the writable options services registered.</returns>
+    public static IServiceCollection AddWritableOptionsCore<T>(this IServiceCollection services)
+        where T : class, new() => services.AddWritableOptionsCore<T>("");
+
+    /// <summary>
+    /// Registers writable options services for the specified options type.
+    /// </summary>
+    /// <typeparam name="T">The type of the options class to configure. This type must be a reference type.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to which the writable options services will be added.</param>
     /// <param name="instanceName">The name of the options instance.</param>
     /// <returns>The <see cref="IServiceCollection"/> instance with the writable options services registered.</returns>
-    public static IServiceCollection AddWritableOptionsCore<T>(this IServiceCollection services, string instanceName)
+    public static IServiceCollection AddWritableOptionsCore<T>(
+        this IServiceCollection services,
+        string instanceName
+    )
         where T : class, new()
     {
         // add IOptionsConfigRegistry<T>
@@ -50,14 +62,17 @@ public static class WritableOptionsCoreExtensions
         // if named instance, add named wrapper
         if (!string.IsNullOrEmpty(instanceName))
         {
-            services.AddKeyedSingleton<IWritableOptions<T>>(instanceName, (p, key) =>
-                new WritableOptionsWithNameImpl<T>(
-                    p.GetRequiredService<WritableOptionsImpl<T>>(),
-                    instanceName
-                )
+            services.AddKeyedSingleton<IWritableOptions<T>>(
+                instanceName,
+                (p, key) =>
+                    new WritableOptionsWithNameImpl<T>(
+                        p.GetRequiredService<WritableOptionsImpl<T>>(),
+                        instanceName
+                    )
             );
-            services.AddKeyedSingleton<IReadOnlyOptions<T>>(instanceName, (p, key) =>
-                p.GetRequiredKeyedService<IWritableOptions<T>>(instanceName)
+            services.AddKeyedSingleton<IReadOnlyOptions<T>>(
+                instanceName,
+                (p, key) => p.GetRequiredKeyedService<IWritableOptions<T>>(instanceName)
             );
         }
         return services;
