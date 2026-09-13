4f6c014ad8f87e474379b71b528bf9961df126b3
arika <Delete0093@Gmail.com>
2026-09-13 17:17:43 +0900

    Preserve unsupported file revision fallback

--- src/Configuration.Writable.Core/Options/WritableOptionsImpl.cs
+++ src/Configuration.Writable.Core/Options/WritableOptionsImpl.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Configuration.Writable.Configure;
 using Configuration.Writable.Diagnostics;
+using Configuration.Writable.FileProvider;
 using Configuration.Writable.Options;
 using Configuration.Writable.State;
 using Microsoft.Extensions.Logging;
@@ -187,6 +188,7 @@ internal sealed class WritableOptionsImpl<T>(
                 options.ConflictResolution == ConfigurationConflictResolution.FailOnConflict
                 && expectedRevision is null
                 && UsesFileWriteTarget(stateSource)
+                && options.FileProvider is IPhysicalFileProvider
             )
             {
                 ConfigurationWritableEventSource.Log.ConflictDetected();
